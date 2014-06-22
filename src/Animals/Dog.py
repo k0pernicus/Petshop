5da3e7109984735912b830178fa894f9fa41e8dc
WebTogz <antonin.carette@gmail.com>
2014-06-22 11:57:28 +0200

    Modification 'Dog.py' - ajout d'infos + modification classe

--- src/Animals/Dog.py
+++ src/Animals/Dog.py
@@ -2,15 +2,15 @@
 #Auteur: Carette Antonin
 #Classe permettant d'instancier un objet représentant l'animal "Chien"
 
-from Animals import *
+from Animals import Animal
 
-class Dog(Animal):
+class Dog(Animal.Animal):
 	"""Classe permettant d'instancier un objet Dog, représentation l'animal \"Chien\""""
 
-	def __init__(self, pts_de_vie, sexe):
+	def __init__(self, sexe):
 		"""Constructeur d'un objet Dog"""
-		Animal.__init__(self, "Dog", pts_de_vie, sexe)
+		Animal.Animal.__init__(self, "Dog", 50, sexe, 100, 500)
 
 	def __del__(self):
 		"""Destructeur d'un objet Dog"""
-		Animal.__del__(self)
+		Animal.Animal.__del__(self)
